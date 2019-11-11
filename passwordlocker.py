ad30809c7474cbf802e6fcb260ab99d90ffc1ca7
BradOdera <bradodera@gmail.com>
2019-11-11 10:23:24 +0300

    create a function for creating a new user

--- passwordlocker.py
+++ passwordlocker.py
@@ -1,2 +1,11 @@
 from user import User
 from credentials import Credentials
+
+
+def create_user(username, password):
+    # '''
+    # Function to create a new user
+    # '''
+
+    new_user = User(username, password)
+    return new_user

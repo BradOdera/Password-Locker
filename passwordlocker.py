dd0e52089b3c7d67eb323ddc647496d3c4045219
BradOdera <bradodera@gmail.com>
2019-11-11 10:32:41 +0300

    add function to create credentials

--- passwordlocker.py
+++ passwordlocker.py
@@ -3,14 +3,26 @@ from credentials import Credentials
 
 
 def create_user(username, password):
+    """
+    Function to create a new user
+    """
 
     new_user = User(username, password)
     return new_user
 
 
 def save_user(user):
-    '''
+    """
     function to save user
-    '''
+    """
 
     User.save_user(user)
+
+
+def create_credentials(account, username, password):
+    """
+    Function to create credentials
+    """
+
+    new_credentials = Credentials(account, username, password)
+    return new_credentials

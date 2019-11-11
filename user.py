b6d0694aae8e2ff697f5f24813da0952d3828545
BradOdera <bradodera@gmail.com>
2019-11-11 09:27:56 +0300

    Create user class that takes in user info

---
+++ user.py
@@ -0,0 +1,34 @@
+import random
+import string
+
+
+class User:
+    '''
+    class that takes in user info.
+    '''
+
+    user_details = []
+
+    def __init__(self, username, password):
+        '''
+        Args:
+        username: New username
+        password: New password
+        '''
+
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+        '''
+        saves user details into user details.
+        '''
+        User.user_details.append(self)
+
+    @classmethod
+    def user_detail(cls):
+        '''
+        returns the user details
+        '''
+
+        return cls.user_details

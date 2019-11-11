2efcb3af31aee30c20a77aee3c876c6f4d207d6b
BradOdera <bradodera@gmail.com>
2019-11-11 10:28:34 +0300

    create a function for saving user

--- passwordlocker.py
+++ passwordlocker.py
@@ -3,9 +3,14 @@ from credentials import Credentials
 
 
 def create_user(username, password):
-    # '''
-    # Function to create a new user
-    # '''
 
     new_user = User(username, password)
     return new_user
+
+
+def save_user(user):
+    '''
+    function to save user
+    '''
+
+    User.save_user(user)

3b5df2389763a3afc08bee7216899cdc41ae59a0
BradOdera <bradodera@gmail.com>
2019-11-11 10:19:28 +0300

    Create tests for credentials and user accounts

--- unitest.py
+++ unitest.py
@@ -1,3 +1,77 @@
 import unittest
 from user import User
 from credentials import Credentials
+
+
+class Tests(unittest.TestCase):
+
+    """
+    Test class that defines test cases for the user 
+    class behaviours
+    """
+
+    def setUp(self):
+        """
+        runs before each test case
+        """
+        self.new_user = User('Brad', '12345')
+        self.new_credentials = Credentials('Twitter', 'Brad', '12345')
+
+        def tearDown(self):
+            '''
+        cleans up after each test case has run
+        '''
+
+        Credentials.credentials_list = []
+
+    def test_init(self):
+        '''
+        tests if objects are instatiated correctly
+        '''
+
+        self.assertEqual(self.new_user.username, 'Brad')
+        self.assertEqual(self.new_user.password, '12345')
+
+    def test_save_credentials(self):
+        '''
+        test case to test if the credentials object is saved
+        into credentials list
+        '''
+
+        self.new_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 1)
+
+    def test_save_multiple_credentials(self):
+        '''
+        to check if we can save multiple credentials.
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials('Twitter', 'Brad', '12345')
+        test_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 2)
+
+    def test_delete_credentials(self):
+        '''
+        tests if credentials can be deleted from the
+        credentials list
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials('Twitter', 'Brad', '12345')
+        test_credentials.save_credentials()
+
+        self.new_credentials.delete_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 1)
+
+    def test_display_all_credentials(self):
+        '''
+        returns a list of all contacts saved.
+        '''
+
+        self.assertEqual(Credentials.display_credentials(),
+                         Credentials.credentials_list)
+
+
+if __name__ == '__main__':
+    unittest.main()
